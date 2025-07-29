a261618d807610535a810efb16b495a434bfb1e6
Gabrielkaos <gabrielmontes155@gmail.com>
2025-07-29 19:29:38 +0800

    first layer

--- layers.py
+++ layers.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+
+inputs = np.array([[0.1,0.2],
+                   [0.3,0.1],
+                   [0.2,0.2]])
+weights = np.array([[1.0, 1.2],[2.0, 0.9]])
+bias = np.array([[1.0, 3.0]])
+
+print(inputs.shape)
+print(weights.shape)
+print(bias.shape)
+
+print()
+output = np.dot(inputs,weights) + bias
+print(output)
